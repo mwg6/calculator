7d411bb35b019b5cbd10ddc623dc624c8f8c5342
Max <Max@Max-PC.home>
2018-05-26 17:18:23 -0400

    did this one work mom?

--- Calculator/src/pemdasLogic.java
+++ Calculator/src/pemdasLogic.java
@@ -1,4 +1,46 @@
 
 public class pemdasLogic {
-
+	float trueAnswer;
+	int countParans = 0;
+	int paranLoc = 0;
+	
+	
+	public float pemdasCalc(String equation){
+		//count all parantheses
+		if(equation.contains("("))
+				{
+			for(int i =0; i<equation.length(); i++)
+			{
+				if(equation.charAt(i)=='('){
+					countParans++;
+				}
+				else{
+					//do nothing, cycle
+				}
+			}
+			//retrieve indexes of parantheses
+			int[] paranInd = new int[countParans*2];
+			
+			for(int i =0; i<equation.length(); i++){
+				if(equation.charAt(i)=='('){
+					paranInd[paranLoc] = i;
+					paranLoc++;
+				}
+				else if(equation.charAt(i)==')'){
+					paranInd[paranLoc] = i;
+					paranLoc++;
+				}
+				else{
+					
+				}
+		}
+			
+		}
+		
+		
+		//solve parantheses areas first
+		
+		
+		return trueAnswer;
+	}
 }
